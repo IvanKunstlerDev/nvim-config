1b5d6984eab3e45c7577e84b4900fb34d049da25
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-12-05 21:33:27 -0300

    configuracion de compatibilidad con tmux.

--- lua/ivankunstlerdev/plugins/utils.lua
+++ lua/ivankunstlerdev/plugins/utils.lua
@@ -24,4 +24,34 @@ return {
 		opts = {},
 		config = true,
 	},
+
+	{
+		"aserowy/tmux.nvim",
+		opts = {
+			copy_sync = {
+				enable = false,
+			},
+			navigation = {
+				cycle_navigation = true,
+				enable_default_keybindings = true,
+				persist_zoom = false,
+			},
+			resize = {
+				enable_default_keybindings = false,
+				resize_step_x = 2,
+				resize_step_y = 2,
+			},
+			swap = {
+				enable_default_keybindings = false,
+			},
+		},
+		config = function(_, opts)
+			require("tmux").setup(opts)
+			local map = vim.keymap.set
+			map("n", "<C-M-h>", "<cmd>lua require('tmux').resize_left()<cr>")
+			map("n", "<C-M-j>", "<cmd>lua require('tmux').resize_bottom()<cr>")
+			map("n", "<C-M-k>", "<cmd>lua require('tmux').resize_top()<cr>")
+			map("n", "<C-M-l>", "<cmd>lua require('tmux').resize_right()<cr>")
+		end,
+	},
 }
