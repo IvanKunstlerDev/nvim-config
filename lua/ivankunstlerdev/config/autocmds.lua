8537dcec7a4d6ba15ce9fbbb48af939e4167c346
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-12-18 10:05:56 -0300

    se agrego el explorador de archivos nvim-tree.

--- lua/ivankunstlerdev/config/autocmds.lua
+++ lua/ivankunstlerdev/config/autocmds.lua
@@ -39,3 +39,20 @@ vim.api.nvim_create_autocmd("FileType", {
 		end)
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function(data)
+		-- buffer is a real file on the disk
+		local real_file = vim.fn.filereadable(data.file) == 1
+
+		-- buffer is a [No Name]
+		local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+		if not real_file and not no_name then
+			return
+		end
+
+		-- open the tree, find the file but don't focus it
+		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+	end,
+})
--- lua/ivankunstlerdev/plugins/explorer.lua
+++ lua/ivankunstlerdev/plugins/explorer.lua
@@ -1,24 +1,83 @@
 return {
-	"stevearc/oil.nvim",
-	lazy = false,
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {
-		keymaps = {
-			["q"] = { "actions.close", mode = "n" },
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			keymaps = {
+				["q"] = { "actions.close", mode = "n" },
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name)
+					return name == ".git" or name == ".."
+				end,
+			},
 		},
-		view_options = {
-			show_hidden = true,
-			is_always_hidden = function(name)
-				return name == ".git" or name == ".."
-			end,
-		},
-	},
-	config = function(_, opts)
-		require("oil").setup(opts)
+		config = function(_, opts)
+			require("oil").setup(opts)
 
-		local map = vim.keymap.set
-		map("n", "-", "<cmd>Oil<cr>", { desc = "Open file explorer." })
-	end,
+			local map = vim.keymap.set
+			map("n", "-", "<cmd>Oil<cr>", { desc = "Open file explorer." })
+		end,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			filters = { dotfiles = false },
+			disable_netrw = true,
+			hijack_cursor = true,
+			sync_root_with_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = false,
+			},
+			view = {
+				width = 32,
+				preserve_window_proportions = true,
+				signcolumn = "yes",
+			},
+			git = { enable = true },
+			diagnostics = { enable = true },
+			renderer = {
+				root_folder_label = false,
+				highlight_git = "name",
+				highlight_diagnostics = "icon",
+				highlight_hidden = "name",
+				indent_markers = { enable = false },
+				icons = {
+					diagnostics_placement = "signcolumn",
+					show = {
+						folder_arrow = false,
+						git = false,
+					},
+					glyphs = {
+						default = "󰈚",
+						folder = {
+							default = "󰉋",
+							empty = "󰉋",
+							empty_open = "󰉋",
+							open = "󰝰",
+							symlink = "󱁽",
+						},
+						git = { unmerged = "" },
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("nvim-tree").setup(opts)
+			local map = vim.keymap.set
+
+			map("n", "<leader>e", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		end,
+	},
 }
