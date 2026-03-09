7c4d540fedfb2b3ea50aac64e5a2755b1674518b
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2026-03-09 20:58:24 -0300

    se agrego lazygit

--- lua/ivankunstlerdev/plugins/git.lua
+++ lua/ivankunstlerdev/plugins/git.lua
@@ -1,6 +1,28 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = "BufEnter",
-	config = true,
-	opts = {},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 }
