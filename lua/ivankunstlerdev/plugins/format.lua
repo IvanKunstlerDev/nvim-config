7a90ce99f6a7180ebc773ea59fa555127b72b29e
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2026-09-03 10:42:16 -0300

    correccion de Error running Biome ENOENT

--- lua/ivankunstlerdev/plugins/format.lua
+++ lua/ivankunstlerdev/plugins/format.lua
@@ -16,19 +16,28 @@ return {
 	---@module "conform"
 	---@type conform.setupOpts
 	opts = {
+		notify_no_formatters = false,
+		notify_on_error = true,
+
+		default_format_opts = {
+			stop_after_first = true,
+			lsp_format = "fallback",
+			quiet = false,
+		},
+
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_fix", "ruff_format" },
 			rust = { "rustfmt", lsp_format = "fallback" },
-			javascript = { "biome", "prettier", stop_after_first = true },
-			typescript = { "biome", "prettier", stop_after_first = true },
-			javascriptreact = { "biome", "prettier", stop_after_first = true },
-			typescriptreact = { "biome", "prettier", stop_after_first = true },
-			json = { "biome", "prettier", "jsonls", stop_after_first = true },
+			javascript = { "prettier", "biome" },
+			typescript = { "prettier", "biome" },
+			javascriptreact = { "prettier", "biome" },
+			typescriptreact = { "prettier", "biome" },
+			json = { "prettier", "biome", "jsonls" },
 		},
+
 		format_on_save = {
 			timeout_ms = 5000,
-			lsp_format = "fallback",
 		},
 	},
 }
